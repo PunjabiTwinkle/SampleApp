21cb88054aa53826cdf3f8b0750d214bfa5f719b
Jigna Patel <jipa@nodesagency.com>
2019-09-18 14:30:09 +0200

    Initial Commit

---
+++ SampleApp/SampleApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleApp
+//
+//  Created by Jigna Patel on 18.09.19.
+//  Copyright © 2019 Nodes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
